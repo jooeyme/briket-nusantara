3b2a62b6aa8f9110b2acfe87118118c8ab51c07a
Tejo M <mtejo25@gmail.com>
2025-07-07 20:27:36 +0700

    fixed app.js

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { About } from "./components/About";
 import { Cta } from "./components/Cta";
-import { FAQ } from "./components/FAQ";
+// import { FAQ } from "./components/FAQ";
 import { Features } from "./components/Features";
 // import { Footer } from "./components/Footer";
-import { Hero } from "./components/Hero";
+// import { Hero } from "./components/Hero";
 import { HowItWorks } from "./components/HowItWorks";
 // import { Navbar } from "./components/Navbar";
 // import { Newsletter } from "./components/Newsletter";
